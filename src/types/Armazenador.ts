e072315b2d8dfb0dc539d412005b917bc53b6d1a
guifeh <guigasparino4@gmail.com>
2025-01-22 16:12:03 -0300

    Adicionando métodos genericos para armazenar e recuperar dados de uma conta

--- src/types/Armazenador.ts
+++ src/types/Armazenador.ts
@@ -5,7 +5,7 @@ export class Armazenador {
             const valorComoString = JSON.stringify(valor);
             localStorage.setItem(chave, valorComoString);
     }
-    static obter(chave: string, reviver?: (this: any, key: string, value: any) =>any){
+    static obter<T>(chave: string, reviver?: (this: any, key: string, value: any) =>any): T | null {
         const valor = localStorage.getItem(chave);
 
         if(valor===null){
@@ -13,9 +13,9 @@ export class Armazenador {
         }
 
         if(reviver){
-            return JSON.parse(valor, reviver);
+            return JSON.parse(valor, reviver) as T;
         }
-        return JSON.parse(valor);
+        return JSON.parse(valor) as T;
     }
     
 }
