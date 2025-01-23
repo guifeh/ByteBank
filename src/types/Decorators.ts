7d6d0bb54503291601efae35c8dc9042cee64d15
guifeh <guigasparino4@gmail.com>
2025-01-23 13:21:03 -0300

    projeto finalizado

---
+++ src/types/Decorators.ts
@@ -0,0 +1,25 @@
+export function ValidaDebito(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function(valorDoDebito: number) {
+        if (valorDoDebito <= 0) {
+            throw new Error("O valor a ser debitado deve ser maior que zero!");
+        }
+        if (valorDoDebito > this.saldo) {
+            throw new Error("Saldo insuficiente!");
+        }
+        return originalMethod.apply(this, [valorDoDebito]);
+    };
+    return descriptor;
+}
+
+export function validaDeposito(target: any, propertyKey: string, descriptor: PropertyDescriptor){
+    const originalMethod = descriptor.value;
+    descriptor.value = function(valorDoDeposito: number){
+        if(valorDoDeposito <= 0){
+            throw new Error("O valor a ser depositado deve ser maior que zero!");
+        }
+        return originalMethod.apply(this, [valorDoDeposito]);
+    };
+    return descriptor;
+}
